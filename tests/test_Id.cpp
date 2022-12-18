a459c09b0a66098f878bbf2f5d70d2137ff1a635
yutotnh <57719497+yutotnh@users.noreply.github.com>
2022-12-18 20:24:35 +0900

    update: CAN通信の場合のIDを返す関数を追加した  #91 (#92)

---
+++ tests/test_Id.cpp
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+
+#include "Id.h"
+
+using namespace spirit;
+
+TEST(Id, IdCanMotorTest)
+{
+    // 正常系のテスト
+    // モーターの数が1の場合
+    // dip_switchに着目
+    EXPECT_EQ(0x200, can::get_motor_id(1, 0, 0x0));
+    EXPECT_EQ(0x27C, can::get_motor_id(1, 0, 0x1F));
+
+    // モーターの数が2の場合
+    // motorに着目
+    EXPECT_EQ(0x2BE, can::get_motor_id(2, 0, 0x1F));
+    EXPECT_EQ(0x2FE, can::get_motor_id(2, 1, 0x1F));
+    // dip_switchに着目(0xFは既にやっているので除外)
+    EXPECT_EQ(0x280, can::get_motor_id(2, 0, 0x0));
+
+    // モーターの数が3/4の場合
+    // motorに着目
+    EXPECT_EQ(0x31F, can::get_motor_id(3, 0, 0x1F));
+    EXPECT_EQ(0x35F, can::get_motor_id(3, 2, 0x1F));
+    EXPECT_EQ(0x31F, can::get_motor_id(4, 0, 0x1F));
+    EXPECT_EQ(0x37F, can::get_motor_id(4, 3, 0x1F));
+    // dip_switchに着目(0xFは既にやっているので除外)
+    EXPECT_EQ(0x300, can::get_motor_id(3, 0, 0x00));
+    EXPECT_EQ(0x300, can::get_motor_id(4, 0, 0x00));
+
+    // 異常系のテスト
+    // dip_switchの値がビット幅( dip_switch_size )を超えている場合
+    EXPECT_EQ(0, can::get_motor_id(1, 0, 0x20));
+
+    // motorの値がモーターの数を超えている場合
+    EXPECT_EQ(0, can::get_motor_id(1, 1, 0x0));
+    EXPECT_EQ(0, can::get_motor_id(2, 2, 0x0));
+    EXPECT_EQ(0, can::get_motor_id(3, 3, 0x0));
+    EXPECT_EQ(0, can::get_motor_id(4, 4, 0x0));
+
+    // 非対応の個数のモーターの場合
+    EXPECT_EQ(0, can::get_motor_id(0, 0, 0x0));
+    EXPECT_EQ(0, can::get_motor_id(5, 0, 0x0));
+    EXPECT_EQ(0, can::get_motor_id(8, 0, 0x0));
+}
