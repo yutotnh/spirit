1241f5979914b9b215b3772c8cefb84c6318fdaf
yutotnh <57719497+yutotnh@users.noreply.github.com>
2023-01-21 20:44:09 +0900

    update: 引数の説明にその引数が入力or出力かを記載した #140

--- include/bit.h
+++ include/bit.h
@@ -18,11 +18,11 @@ uint32_t get_range_value(const uint8_t* buffer, std::size_t buffer_size, std::si
 
 /**
  * @brief バッファの指定した範囲のビットを設定する
- * @param value 値
- * @param start 開始位置(0から始まる)
- * @param value_size 値のビット幅
- * @param buffer_size バッファのバイト幅
- * @param buffer バッファ
+ * @param [in] value 値
+ * @param [in] start 開始位置(0から始まる)
+ * @param [in] value_size 値のビット幅
+ * @param [in] buffer_size バッファのバイト幅
+ * @param [out] buffer バッファ
  * @return 成功したかどうか
  */
 bool set_range_value(uint32_t value, std::size_t start, std::size_t value_size, std::size_t buffer_size,
