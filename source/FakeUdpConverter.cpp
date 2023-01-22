ec29c0f8786b3ef196b46a84e159d7cde0e79f4f
yutotnh <57719497+yutotnh@users.noreply.github.com>
2023-01-23 03:47:18 +0900

    update: PwmDataConverterクラスでstringを使わないように変更した #151

--- source/FakeUdpConverter.cpp
+++ source/FakeUdpConverter.cpp
@@ -19,7 +19,7 @@ bool FakeUdpConverter::encode(const uint8_t* payload, const std::size_t payload_
     buffer_size = payload_size + 1;
     if (max_buffer_size < buffer_size) {
         Error&         error          = Error::get_instance();
-        constexpr char message_base[] = "Buffer size (%d) is larger than the maximum buffer size (%d)";
+        constexpr char message_base[] = "Buffer size (%zu) is larger than the maximum buffer size (%zu)";
         char           message[sizeof(message_base) + Error::max_uint32_t_length * 2];
         snprintf(message, sizeof(message), message_base, buffer_size, max_buffer_size);
         error.warning(Error::Type::IllegalCombination, 0, message, __FILE__, __func__, __LINE__);
@@ -56,7 +56,7 @@ bool FakeUdpConverter::decode(const uint8_t* buffer, const std::size_t buffer_si
     // 最大ペイロードサイズがbuffer_size - 1よりも大きい場合は、デコードできない
     if (max_payload_size < buffer_size - 1) {
         Error&         error          = Error::get_instance();
-        constexpr char message_base[] = "Payload size (%d) is larger than the maximum payload size (%d)";
+        constexpr char message_base[] = "Payload size (%zu) is larger than the maximum payload size (%zu)";
         char           message[sizeof(message_base) + Error::max_uint32_t_length * 2];
         snprintf(message, sizeof(message), message_base, buffer_size - 1, max_payload_size);
         error.warning(Error::Type::IllegalCombination, 0, message, __FILE__, __func__, __LINE__);
