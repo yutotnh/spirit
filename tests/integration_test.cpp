52bd031fa1165bb66597d7ecc318f18496e71db9
yutotnh <57719497+yutotnh@users.noreply.github.com>
2023-01-28 21:31:01 +0900

    update: 結合テスト部を速度制御に対応させた #167 (#168)

--- tests/integration_test.cpp
+++ tests/integration_test.cpp
@@ -3,7 +3,7 @@
 #include "FakeUdpConverter.h"
 #include "Id.h"
 #include "Motor.h"
-#include "PwmDataConverter.h"
+#include "MotorDataConverter.h"
 #include "include/Error.h"
 
 namespace {
@@ -22,22 +22,22 @@ TEST(IntegrationTest, 1)
     constexpr uint32_t dip_sw = 0x00;
 
     /// 送信側
-    auto controller = [&dip_sw](spirit::Motor &motor) {
-        spirit::FakeUdpConverter fake_udp;
-        spirit::PwmDataConverter pwm_data;
-        const uint32_t           can_id = spirit::can::get_motor_id(1, 0, dip_sw);
+    auto controller = [&dip_sw](const spirit::Motor &motor) {
+        spirit::FakeUdpConverter   fake_udp;
+        spirit::MotorDataConverter motor_data;
+        const uint32_t             can_id = spirit::can::get_motor_id(1, 0, dip_sw);
 
-        constexpr std::size_t max_pwm_data_buffer_size = 64;
-        uint8_t               pwm_data_buffer[8]       = {};
-        std::size_t           pwm_data_buffer_size;
+        constexpr std::size_t max_motor_data_buffer_size = 64;
+        uint8_t               motor_data_buffer[8]       = {};
+        std::size_t           motor_data_buffer_size;
 
         constexpr std::size_t max_fake_udp_buffer_size = 64;
         uint8_t               fake_udp_buffer[8]       = {};
         std::size_t           fake_udp_buffer_size;
 
-        pwm_data.encode(motor, max_pwm_data_buffer_size, pwm_data_buffer, pwm_data_buffer_size);
+        motor_data.encode(motor, max_motor_data_buffer_size, motor_data_buffer, motor_data_buffer_size);
 
-        fake_udp.encode(pwm_data_buffer, pwm_data_buffer_size, max_fake_udp_buffer_size, fake_udp_buffer,
+        fake_udp.encode(motor_data_buffer, motor_data_buffer_size, max_fake_udp_buffer_size, fake_udp_buffer,
                         fake_udp_buffer_size);
 
         ::CANMessage message;
@@ -55,8 +55,8 @@ TEST(IntegrationTest, 1)
 
     /// 受信側
     auto peripheral = [&dip_sw](const ::CANMessage &message) {
-        spirit::FakeUdpConverter fake_udp;
-        spirit::PwmDataConverter pwm_data;
+        spirit::FakeUdpConverter   fake_udp;
+        spirit::MotorDataConverter motor_data;
 
         const uint32_t can_id = spirit::can::get_motor_id(1, 0, dip_sw);
 
@@ -68,40 +68,82 @@ TEST(IntegrationTest, 1)
         fake_udp.decode(message.data, message.len * 8, max_payload_size, payload, payload_size);
 
         spirit::Motor motor;
-        pwm_data.decode(payload, payload_size, motor);
+        motor_data.decode(payload, payload_size, motor);
 
         return motor;
     };
 
-    /// テスト
-    auto test = [&controller, &peripheral](spirit::Motor::State state, float duty_cycle) {
+    /// PWM制御
+    auto pwm_test = [&controller, &peripheral](const spirit::Motor::State state, const float duty_cycle) {
         spirit::Motor motor;
-        motor.state(state);
+        motor.control_system(spirit::Motor::ControlSystem::PWM);
         motor.duty_cycle(duty_cycle);
+        motor.state(state);
 
         ::CANMessage  message        = controller(motor);
         spirit::Motor received_motor = peripheral(message);
 
         // デューティー比は16bitで表現しているため、1/65535の誤差を許容する
         constexpr float allowable_error_margin = 1.0F / 65535.0F;
+        EXPECT_EQ(spirit::Motor::ControlSystem::PWM, received_motor.get_control_system());
         EXPECT_EQ(state, received_motor.get_state());
         EXPECT_NEAR(duty_cycle, received_motor.get_duty_cycle(), allowable_error_margin);
+    };
+
+    auto speed_test = [&controller, &peripheral](const spirit::Motor::State state, const float speed, const float Kp,
+                                                 const float Ki, const float Kd) {
+        spirit::Motor motor;
+        motor.control_system(spirit::Motor::ControlSystem::Speed);
+        motor.state(state);
+        motor.speed(speed);
+        motor.pid_gain_factor(Kp, Ki, Kd);
+
+        ::CANMessage  message        = controller(motor);
+        spirit::Motor received_motor = peripheral(message);
+
+        // スピード, Kp, Kiは送受信中にbfloat16に変換している
+        // bfloat16の仮数部は7bitなので、その分の誤差を考慮する必要がある
+        auto allowable_error_margin = [](const float bfloat16) { return bfloat16 / 127.0F; };
+
+        EXPECT_EQ(spirit::Motor::ControlSystem::Speed, received_motor.get_control_system());
+        EXPECT_EQ(state, received_motor.get_state());
+        EXPECT_NEAR(speed, motor.get_speed(), allowable_error_margin(speed));
+
+        float return_Kp, return_Ki, return_Kd;
+        received_motor.get_pid_gain_factor(return_Kp, return_Ki, return_Kd);
+        EXPECT_NEAR(Kp, return_Kp, allowable_error_margin(Kp));
+        EXPECT_NEAR(Ki, return_Ki, allowable_error_margin(Ki));
     };
 
     spirit::Error &error = spirit::Error::get_instance();
     EXPECT_EQ(error.get_status(), spirit::Error::Status::Normal);
 
+    // PWM制御のテスト
+
     /// @test state = Brake, doty_cycle = 0.75
-    test(spirit::Motor::State::Coast, 0.75F);
+    pwm_test(spirit::Motor::State::Coast, 0.75F);
 
     /// @test state = Brake, doty_cycle = 0.2
-    test(spirit::Motor::State::Brake, 0.2F);
+    pwm_test(spirit::Motor::State::Brake, 0.2F);
 
     /// @test state = CW, doty_cycle = 0.5
-    test(spirit::Motor::State::CW, 0.5F);
+    pwm_test(spirit::Motor::State::CW, 0.5F);
 
     /// @test state = CCW, doty_cycle = 1.0
-    test(spirit::Motor::State::CCW, 1.0F);
+    pwm_test(spirit::Motor::State::CCW, 1.0F);
+
+    // 速度制御(通信の都合上Kdの値は無視される)
+    /// @test state = Brake, speed = 0.75, Kp = 0.2, Ki = 0.5, Kd = 1.0
+    speed_test(spirit::Motor::State::Coast, 0.75F, 0.2F, 0.5F, 1.0F);
+
+    /// @test state = Brake, speed = 0.2, Kp = 0.5, Ki = 1.0, Kd = 0.2
+    speed_test(spirit::Motor::State::Brake, 0.2F, 0.5F, 1.0F, 0.2F);
+
+    /// @test state = CW, speed = 0.5, Kp = 1.0, Ki = 0.2, Kd = 0.5
+    speed_test(spirit::Motor::State::CW, 0.5F, 1.0F, 0.2F, 0.5F);
+
+    /// @test state = CCW, speed = 1.0, Kp = 0.2, Ki = 0.5, Kd = 1.0
+    speed_test(spirit::Motor::State::CCW, 1.0F, 0.2F, 0.5F, 1.0F);
 
     EXPECT_EQ(error.get_status(), spirit::Error::Status::Normal);
 }
