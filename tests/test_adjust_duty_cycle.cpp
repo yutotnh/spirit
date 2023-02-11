1f0ce5167ba079e9d9775c3e04007adfc8ba3163
yutotnh <57719497+yutotnh@users.noreply.github.com>
2023-02-12 02:43:30 +0900

    update: デューティー比の最大変化幅を手動で設定した場合のテストを追加した #173

--- tests/test_adjust_duty_cycle.cpp
+++ tests/test_adjust_duty_cycle.cpp
@@ -135,4 +135,43 @@ TEST(AdjustDutyCycle, SameState)
     }
 }
 
+/**
+ * @brief デューティー比の最大変化幅を手動で設定した場合のテスト
+ */
+TEST(AdjustDutyCycle, ManualChangeLevel)
+{
+    /// @test Motor::ChangeLevel::Manual の時にデューティー比が変更されることの確認
+    {
+        Motor motor;
+        motor.control_system(Motor::ControlSystem::PWM);
+        motor.state(Motor::State::CW);
+        motor.duty_cycle(0.80F);
+
+        float change_level_fall = 0.10F;
+        float change_level_rise = 0.15F;
+
+        motor.change_level(Motor::ChangeLevelTarget::Fall, change_level_fall);
+        motor.change_level(Motor::ChangeLevelTarget::Rise, change_level_rise);
+
+        Motor::State current_state;
+        float        current_duty_cycle;
+
+        Motor::State nest_state;
+        float        nest_duty_cycle;
+
+        current_state      = Motor::State::CW;
+        current_duty_cycle = 1.00F;
+
+        adjust_duty_cycle(motor, current_state, current_duty_cycle, nest_state, nest_duty_cycle);
+        EXPECT_EQ(nest_state, current_state);
+        EXPECT_FLOAT_EQ(nest_duty_cycle, current_duty_cycle - change_level_fall);
+
+        current_state      = Motor::State::CW;
+        current_duty_cycle = 0.50F;
+        adjust_duty_cycle(motor, current_state, current_duty_cycle, nest_state, nest_duty_cycle);
+        EXPECT_EQ(nest_state, current_state);
+        EXPECT_FLOAT_EQ(nest_duty_cycle, current_duty_cycle + change_level_rise);
+    }
+}
+
 }  // namespace
