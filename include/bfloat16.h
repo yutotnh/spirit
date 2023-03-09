9b64df44e533d452446db29feddb89f89649aa88
yutotnh <57719497+yutotnh@users.noreply.github.com>
2023-03-09 23:27:54 +0900

    fix: コメントの文面修正と必要のない引数の const の削除をした (#178)

--- include/bfloat16.h
+++ include/bfloat16.h
@@ -9,13 +9,13 @@ namespace spirit {
  * @param [in] bfloat16 変換元のbfloat16
  * @return 変換結果のfloat32
 */
-float bfloat16_to_float32(const uint16_t bfloat16);
+float bfloat16_to_float32(uint16_t bfloat16);
 /**
  * @brief float32をbfloat16に変換する
  * @param [in] value 変換元のfloat32
  * @return 変換結果のbfloat16(型はuint16_t)
 */
-uint16_t float32_to_bfloat16(const float value);
+uint16_t float32_to_bfloat16(float value);
 }  // namespace spirit
 
 #endif  // SPIRIT_BFLOAT16_H
