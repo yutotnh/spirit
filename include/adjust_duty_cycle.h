0dacf51a6e8299b6baf07161d1a30a13ad935178
yutotnh <57719497+yutotnh@users.noreply.github.com>
2023-03-13 22:21:38 +0900

    update: 回転方向がCoast or Brakeの場合は現在のデューティー比を0として扱う #180

--- include/adjust_duty_cycle.h
+++ include/adjust_duty_cycle.h
@@ -25,15 +25,10 @@ void adjust_duty_cycle(spirit::Motor::State target_state, float target_duty_cycl
                        float max_fall_delta, spirit::Motor::State current_state, float current_duty_cycle,
                        spirit::Motor::State& next_state, float& next_duty_cycle)
 {
-    // Coast <=> Brake, Coast <=> Coast, Brake <=> Brake の場合はデューティー比を0にする
-    if (((current_state == Motor::State::Coast) || (current_state == Motor::State::Brake)) &&
-        ((target_state == Motor::State::Coast) || (target_state == Motor::State::Brake))) {
-        next_state      = target_state;
-        next_duty_cycle = 0.00F;
-        return;
+    // 回転方向がCoast or Brakeの場合は現在のデューティー比を0として扱う
+    if ((target_state == Motor::State::Coast) || (target_state == Motor::State::Brake)) {
+        target_duty_cycle = 0.00F;
     }
-
-    // 現在の回転方向がCoast or Brakeの場合は現在のデューティー比を0として扱う
     if ((current_state == Motor::State::Coast) || (current_state == Motor::State::Brake)) {
         current_duty_cycle = 0.00F;
     }
