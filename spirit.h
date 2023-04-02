3bb9e07f4b1d14dbd8782b756e7297337bdfff57
yutotnh <57719497+yutotnh@users.noreply.github.com>
2023-04-02 23:16:58 +0900

    update: spirit.h を作成した #179

---
+++ spirit.h
@@ -0,0 +1,25 @@
+#ifndef SPIRIT_H
+
+#include "include/A3921.h"
+#include "include/CANMessage.h"
+#include "include/Error.h"
+#include "include/FakeUdpConverter.h"
+#include "include/Id.h"
+#include "include/InterfaceDigitalOut.h"
+#include "include/InterfacePwmOut.h"
+#include "include/MdLed.h"
+#include "include/Motor.h"
+#include "include/MotorDataConverter.h"
+#include "include/PwmDataConverter.h"
+#include "include/SpeedDataConverter.h"
+#include "include/adjust_duty_cycle.h"
+#include "include/bfloat16.h"
+#include "include/bit.h"
+// #include "include/mutex.h"
+
+#ifdef MBED_H
+#include "platform/mbed/include/DigitalOut.h"
+#include "platform/mbed/include/PwmOut.h"
+#endif  // MBED_H
+
+#endif  //  SPIRIT_H
