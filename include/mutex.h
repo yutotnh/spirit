4471fb5539661eaa374aabbae6b8fc2fb8d647d2
yutotnh <57719497+yutotnh@users.noreply.github.com>
2023-04-09 11:56:09 +0900

    fix: マクロ定義されているかを if のみでみていたが、 if + defined で確認させる

--- include/mutex.h
+++ include/mutex.h
@@ -1,7 +1,7 @@
 #ifndef SPIRIT_MUTEX_H
 #define SPIRIT_MUTEX_H
 
-#if SPIRIT_MBED || __MBED__
+#if defined(SPIRIT_MBED) || defined(__MBED__)
 #include "mbed.h"
 #else
 #include <mutex>
@@ -19,7 +19,7 @@ public:
     void unlock();
 
 private:
-#if SPIRIT_MBED || __MBED__
+#if defined(SPIRIT_MBED) || defined(__MBED__)
     Mutex _mutex;
 #else
     std::mutex _mutex;
--- spirit.h
+++ spirit.h
@@ -18,7 +18,7 @@
 #include "include/bit.h"
 #include "include/mutex.h"
 
-#ifdef MBED_H
+#if defined(SPIRIT_MBED) || defined(__MBED__)
 #include "platform/mbed/include/DigitalOut.h"
 #include "platform/mbed/include/PwmOut.h"
 #endif  // MBED_H
