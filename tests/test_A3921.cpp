bc37fdce07ed23cd039cfbd1dd35ebd2a01042d0
yutotnh <57719497+yutotnh@users.noreply.github.com>
2023-07-26 20:16:33 +0900

    refactor: テストコードを改善 (#246)
    * refactor: A3921のテストコードを改善
    
    * refactor: Motor のテストコードを改善
    
    * fix: typo
    
    * style: format

--- tests/test_A3921.cpp
+++ tests/test_A3921.cpp
@@ -58,6 +58,10 @@ TEST(A3921, InitValueTest)
     EXPECT_FLOAT_EQ(phase.read_period(), Motor::Default::pulse_period);
 
     EXPECT_TRUE(is_brake(pwmh, pwml));
+
+    // Errorが発生していないことを確認
+    Error& error = Error::get_instance();
+    EXPECT_EQ(error.get_status(), spirit::Error::Status::Normal);
 }
 
 /**
@@ -78,6 +82,10 @@ TEST(A3921, SleepTest)
 
     a3921.sleep(false);
     EXPECT_EQ(reset.read(), 1);
+
+    // Errorが発生していないことを確認
+    Error& error = Error::get_instance();
+    EXPECT_EQ(error.get_status(), spirit::Error::Status::Normal);
 }
 
 /**
@@ -111,6 +119,10 @@ TEST(A3921, ResetTest)
 
     EXPECT_EQ(reset.read(), 1);
     EXPECT_LE(100.0e-6, elapsed_seconds.count());
+
+    // Errorが発生していないことを確認
+    Error& error = Error::get_instance();
+    EXPECT_EQ(error.get_status(), spirit::Error::Status::Normal);
 }
 
 /**
@@ -120,47 +132,77 @@ TEST(A3921, ResetTest)
  */
 TEST(A3921, SlowDecayLowSideTest)
 {
-    StubDigitalOut sr;
-    StubPwmOut     pwmh;
-    StubPwmOut     pwml;
-    StubPwmOut     phase;
-    StubDigitalOut reset;
-    A3921          a3921(sr, pwmh, pwml, phase, reset);
+    auto test = [](float duty_cycle, float expected_duty_cycle, Error::Status expected_status) {
+        Error& error = Error::get_instance();
+        error.reset();
+        EXPECT_EQ(error.get_status(), Error::Status::Normal);
 
-    // 共通の設定
-    a3921.decay(Motor::Decay::Slow);
-    a3921.pwm_side(Motor::PwmSide::Low);
-    a3921.duty_cycle(0.50F);
+        if (expected_status != Error::Status::Normal) {
+            // Error時に標準エラー出力に文字列が出力される
+            // 本当のエラー時にエラー出力させたいので、異常系のテスト中は標準エラー出力をキャプチャする
+            testing::internal::CaptureStderr();
+        }
 
-    // Coast
-    a3921.state(Motor::State::Coast);
-    a3921.run();
+        StubDigitalOut sr;
+        StubPwmOut     pwmh;
+        StubPwmOut     pwml;
+        StubPwmOut     phase;
+        StubDigitalOut reset;
+        A3921          a3921(sr, pwmh, pwml, phase, reset);
 
-    EXPECT_TRUE(is_coast(pwmh, pwml));
+        // 共通の設定
+        a3921.decay(Motor::Decay::Slow);
+        a3921.pwm_side(Motor::PwmSide::Low);
+        a3921.duty_cycle(duty_cycle);
 
-    // CW
-    a3921.state(Motor::State::CW);
-    a3921.run();
+        // Coast
+        a3921.state(Motor::State::Coast);
+        a3921.run();
 
-    EXPECT_EQ(sr.read(), 1);
-    EXPECT_FLOAT_EQ(pwmh.read(), 1.00F);
-    EXPECT_FLOAT_EQ(pwml.read(), 0.50F);
-    EXPECT_FLOAT_EQ(phase.read(), 1.00F);
+        EXPECT_TRUE(is_coast(pwmh, pwml));
 
-    // CCW
-    a3921.state(Motor::State::CCW);
-    a3921.run();
+        // CW
+        a3921.state(Motor::State::CW);
+        a3921.run();
 
-    EXPECT_EQ(sr.read(), 1);
-    EXPECT_FLOAT_EQ(pwmh.read(), 1.00F);
-    EXPECT_FLOAT_EQ(pwml.read(), 0.50F);
-    EXPECT_FLOAT_EQ(phase.read(), 0.00F);
+        EXPECT_EQ(sr.read(), 1);
+        EXPECT_FLOAT_EQ(pwmh.read(), 1.00F);
+        EXPECT_FLOAT_EQ(pwml.read(), expected_duty_cycle);
+        EXPECT_FLOAT_EQ(phase.read(), 1.00F);
 
-    // Brake
-    a3921.state(Motor::State::Brake);
-    a3921.run();
+        // CCW
+        a3921.state(Motor::State::CCW);
+        a3921.run();
 
-    EXPECT_TRUE(is_brake(pwmh, pwml));
+        EXPECT_EQ(sr.read(), 1);
+        EXPECT_FLOAT_EQ(pwmh.read(), 1.00F);
+        EXPECT_FLOAT_EQ(pwml.read(), expected_duty_cycle);
+        EXPECT_FLOAT_EQ(phase.read(), 0.00F);
+
+        // Brake
+        a3921.state(Motor::State::Brake);
+        a3921.run();
+
+        EXPECT_TRUE(is_brake(pwmh, pwml));
+
+        EXPECT_EQ(error.get_status(), expected_status);
+
+        if (expected_status != Error::Status::Normal) {
+            testing::internal::GetCapturedStderr();
+        }
+    };
+
+    test(0.00F, 0.00F, Error::Status::Normal);
+    test(0.01F, 0.01F, Error::Status::Normal);
+    test(0.50F, 0.50F, Error::Status::Normal);
+    test(0.99F, 0.99F, Error::Status::Normal);
+    test(1.00F, 1.00F, Error::Status::Normal);
+
+    // デューティー比が範囲外(x < 0.00, 1.00 < x)のときに境界値になることを確認
+    test(-0.01F, 0.00F, Error::Status::Warning);
+    test(-1.00F, 0.00F, Error::Status::Warning);
+    test(1.01F, 1.00F, Error::Status::Warning);
+    test(2.01F, 1.00F, Error::Status::Warning);
 }
 
 /**
@@ -170,47 +212,77 @@ TEST(A3921, SlowDecayLowSideTest)
  */
 TEST(A3921, SlowDecayHighSideTest)
 {
-    StubDigitalOut sr;
-    StubPwmOut     pwmh;
-    StubPwmOut     pwml;
-    StubPwmOut     phase;
-    StubDigitalOut reset;
-    A3921          a3921(sr, pwmh, pwml, phase, reset);
+    auto test = [](float duty_cycle, float expected_duty_cycle, Error::Status expected_status) {
+        Error& error = Error::get_instance();
+        error.reset();
+        EXPECT_EQ(error.get_status(), Error::Status::Normal);
 
-    // 共通の設定
-    a3921.decay(Motor::Decay::Slow);
-    a3921.pwm_side(Motor::PwmSide::High);
-    a3921.duty_cycle(0.50F);
+        if (expected_status != Error::Status::Normal) {
+            // Error時に標準エラー出力に文字列が出力される
+            // 本当のエラー時にエラー出力させたいので、異常系のテスト中は標準エラー出力をキャプチャする
+            testing::internal::CaptureStderr();
+        }
 
-    // Coast
-    a3921.state(Motor::State::Coast);
-    a3921.run();
+        StubDigitalOut sr;
+        StubPwmOut     pwmh;
+        StubPwmOut     pwml;
+        StubPwmOut     phase;
+        StubDigitalOut reset;
+        A3921          a3921(sr, pwmh, pwml, phase, reset);
 
-    EXPECT_TRUE(is_coast(pwmh, pwml));
+        // 共通の設定
+        a3921.decay(Motor::Decay::Slow);
+        a3921.pwm_side(Motor::PwmSide::High);
+        a3921.duty_cycle(duty_cycle);
 
-    // CW
-    a3921.state(Motor::State::CW);
-    a3921.run();
+        // Coast
+        a3921.state(Motor::State::Coast);
+        a3921.run();
 
-    EXPECT_EQ(sr.read(), 1);
-    EXPECT_FLOAT_EQ(pwmh.read(), 0.50F);
-    EXPECT_FLOAT_EQ(pwml.read(), 1.00F);
-    EXPECT_FLOAT_EQ(phase.read(), 1.00F);
+        EXPECT_TRUE(is_coast(pwmh, pwml));
 
-    // CCW
-    a3921.state(Motor::State::CCW);
-    a3921.run();
+        // CW
+        a3921.state(Motor::State::CW);
+        a3921.run();
 
-    EXPECT_EQ(sr.read(), 1);
-    EXPECT_FLOAT_EQ(pwmh.read(), 0.50F);
-    EXPECT_FLOAT_EQ(pwml.read(), 1.00F);
-    EXPECT_FLOAT_EQ(phase.read(), 0.00F);
+        EXPECT_EQ(sr.read(), 1);
+        EXPECT_FLOAT_EQ(pwmh.read(), expected_duty_cycle);
+        EXPECT_FLOAT_EQ(pwml.read(), 1.00F);
+        EXPECT_FLOAT_EQ(phase.read(), 1.00F);
 
-    // Brake
-    a3921.state(Motor::State::Brake);
-    a3921.run();
+        // CCW
+        a3921.state(Motor::State::CCW);
+        a3921.run();
 
-    EXPECT_TRUE(is_brake(pwmh, pwml));
+        EXPECT_EQ(sr.read(), 1);
+        EXPECT_FLOAT_EQ(pwmh.read(), expected_duty_cycle);
+        EXPECT_FLOAT_EQ(pwml.read(), 1.00F);
+        EXPECT_FLOAT_EQ(phase.read(), 0.00F);
+
+        // Brake
+        a3921.state(Motor::State::Brake);
+        a3921.run();
+
+        EXPECT_TRUE(is_brake(pwmh, pwml));
+
+        EXPECT_EQ(error.get_status(), expected_status);
+
+        if (expected_status != Error::Status::Normal) {
+            testing::internal::GetCapturedStderr();
+        }
+    };
+
+    test(0.00F, 0.00F, Error::Status::Normal);
+    test(0.01F, 0.01F, Error::Status::Normal);
+    test(0.50F, 0.50F, Error::Status::Normal);
+    test(0.99F, 0.99F, Error::Status::Normal);
+    test(1.00F, 1.00F, Error::Status::Normal);
+
+    // デューティー比が範囲外(x < 0.00, 1.00 < x)のときに境界値になることを確認
+    test(-0.01F, 0.00F, Error::Status::Warning);
+    test(-1.00F, 0.00F, Error::Status::Warning);
+    test(1.01F, 1.00F, Error::Status::Warning);
+    test(2.01F, 1.00F, Error::Status::Warning);
 }
 
 /**
@@ -234,7 +306,17 @@ TEST(A3921, MixedDecayTest)
 
     Error& error = Error::get_instance();
     EXPECT_EQ(error.get_status(), Error::Status::Normal);
+
+    /// @test Motor::Decay::Mixed は現在は未対応なので、Error が発生することを確認する
+    error.reset();
+    EXPECT_EQ(error.get_status(), Error::Status::Normal);
     a3921.decay(Motor::Decay::Mixed);
+    EXPECT_EQ(error.get_status(), Error::Status::Error);
+
+    /// @test ついでに Decay が範囲外の場合もテスト
+    error.reset();
+    EXPECT_EQ(error.get_status(), Error::Status::Normal);
+    a3921.decay(static_cast<Motor::Decay>(3));
     EXPECT_EQ(error.get_status(), Error::Status::Error);
 
     testing::internal::GetCapturedStderr();
@@ -247,52 +329,82 @@ TEST(A3921, MixedDecayTest)
  */
 TEST(A3921, FastDecayTest)
 {
-    StubDigitalOut sr;
-    StubPwmOut     pwmh;
-    StubPwmOut     pwml;
-    StubPwmOut     phase;
-    StubDigitalOut reset;
-    A3921          a3921(sr, pwmh, pwml, phase, reset);
+    auto test = [](float duty_cycle, float expected_duty_cycle, Error::Status expected_status) {
+        Error& error = Error::get_instance();
+        error.reset();
+        EXPECT_EQ(error.get_status(), Error::Status::Normal);
 
-    // 共通の設定
-    a3921.decay(Motor::Decay::Fast);
-    a3921.duty_cycle(0.50F);
+        if (expected_status != Error::Status::Normal) {
+            // Error時に標準エラー出力に文字列が出力される
+            // 本当のエラー時にエラー出力させたいので、異常系のテスト中は標準エラー出力をキャプチャする
+            testing::internal::CaptureStderr();
+        }
 
-    // Coast
-    a3921.state(Motor::State::Coast);
-    a3921.run();
+        StubDigitalOut sr;
+        StubPwmOut     pwmh;
+        StubPwmOut     pwml;
+        StubPwmOut     phase;
+        StubDigitalOut reset;
+        A3921          a3921(sr, pwmh, pwml, phase, reset);
 
-    EXPECT_TRUE(is_coast(pwmh, pwml));
+        // 共通の設定
+        a3921.decay(Motor::Decay::Fast);
+        a3921.duty_cycle(duty_cycle);
 
-    // CW
-    a3921.state(Motor::State::CW);
-    a3921.run();
+        // Coast
+        a3921.state(Motor::State::Coast);
+        a3921.run();
 
-    EXPECT_EQ(sr.read(), 0);
-    EXPECT_FLOAT_EQ(pwmh.read(), 1.00F);
-    EXPECT_FLOAT_EQ(pwml.read(), 1.00F);
-    EXPECT_FLOAT_EQ(phase.read(), 0.75F);
+        EXPECT_TRUE(is_coast(pwmh, pwml));
 
-    // CCW
-    a3921.state(Motor::State::CCW);
-    a3921.run();
+        // CW
+        a3921.state(Motor::State::CW);
+        a3921.run();
 
-    EXPECT_EQ(sr.read(), 0);
-    EXPECT_FLOAT_EQ(pwmh.read(), 1.00F);
-    EXPECT_FLOAT_EQ(pwml.read(), 1.00F);
-    EXPECT_FLOAT_EQ(phase.read(), 0.25F);
+        EXPECT_EQ(sr.read(), 0);
+        EXPECT_FLOAT_EQ(pwmh.read(), 1.00F);
+        EXPECT_FLOAT_EQ(pwml.read(), 1.00F);
+        EXPECT_FLOAT_EQ(phase.read(), 0.50F + (expected_duty_cycle / 2.0F));
 
-    // Brake
-    a3921.state(Motor::State::Brake);
-    a3921.run();
+        // CCW
+        a3921.state(Motor::State::CCW);
+        a3921.run();
 
-    EXPECT_TRUE(is_brake(pwmh, pwml));
+        EXPECT_EQ(sr.read(), 0);
+        EXPECT_FLOAT_EQ(pwmh.read(), 1.00F);
+        EXPECT_FLOAT_EQ(pwml.read(), 1.00F);
+        EXPECT_FLOAT_EQ(phase.read(), 0.50F - (expected_duty_cycle / 2.0F));
+
+        // Brake
+        a3921.state(Motor::State::Brake);
+        a3921.run();
+
+        EXPECT_TRUE(is_brake(pwmh, pwml));
+
+        EXPECT_EQ(error.get_status(), expected_status);
+
+        if (expected_status != Error::Status::Normal) {
+            testing::internal::GetCapturedStderr();
+        }
+    };
+
+    test(0.00F, 0.00F, Error::Status::Normal);
+    test(0.01F, 0.01F, Error::Status::Normal);
+    test(0.50F, 0.50F, Error::Status::Normal);
+    test(0.99F, 0.99F, Error::Status::Normal);
+    test(1.00F, 1.00F, Error::Status::Normal);
+
+    /// @test デューティー比が範囲外(x < 0.00, 1.00 < x)のときに付近の境界値になることを確認
+    test(-0.01F, 0.00F, Error::Status::Warning);
+    test(-1.00F, 0.00F, Error::Status::Warning);
+    test(1.01F, 1.00F, Error::Status::Warning);
+    test(2.01F, 1.00F, Error::Status::Warning);
 }
 
 /**
  * @brief A3921の各関数の Error が発生する条件のテスト
  */
-TEST(A3921, NonNormalTest)
+TEST(A3921, AnomalyTest)
 {
     StubDigitalOut sr;
     StubPwmOut     pwmh;
@@ -307,17 +419,35 @@ TEST(A3921, NonNormalTest)
 
     Error& error = Error::get_instance();
     EXPECT_EQ(error.get_status(), Error::Status::Normal);
+
+    /// @test デューティー比が1.00を超えると警告を出すことのテスト
+    error.reset();
+    EXPECT_EQ(error.get_status(), Error::Status::Normal);
+    a3921.duty_cycle(1.01F);
+    EXPECT_EQ(error.get_status(), Error::Status::Warning);
+
+    /// @test デューティー比が0.00を下回ると警告を出すことのテスト
+    error.reset();
+    EXPECT_EQ(error.get_status(), Error::Status::Normal);
+    a3921.duty_cycle(-0.01F);
+    EXPECT_EQ(error.get_status(), Error::Status::Warning);
+
+    /// @test Motor::State が範囲外の時にエラーになることのテスト
+    error.reset();
+    EXPECT_EQ(error.get_status(), Error::Status::Normal);
     a3921.state(static_cast<Motor::State>(4));
     EXPECT_EQ(error.get_status(), Error::Status::Error);
 
-    error.reset();
-    EXPECT_EQ(error.get_status(), Error::Status::Normal);
-    a3921.pwm_side(static_cast<Motor::PwmSide>(3));
-    EXPECT_EQ(error.get_status(), Error::Status::Error);
-
+    /// @test Motor::Decay が範囲外の時にエラーになることのテスト
     error.reset();
     EXPECT_EQ(error.get_status(), Error::Status::Normal);
     a3921.decay(static_cast<Motor::Decay>(3));
+    EXPECT_EQ(error.get_status(), Error::Status::Error);
+
+    /// @test Motor::PwmSide が範囲外の時にエラーになることのテスト
+    error.reset();
+    EXPECT_EQ(error.get_status(), Error::Status::Normal);
+    a3921.pwm_side(static_cast<Motor::PwmSide>(3));
     EXPECT_EQ(error.get_status(), Error::Status::Error);
 
     testing::internal::GetCapturedStderr();
@@ -343,14 +473,14 @@ TEST(A3921, PulsePeriodTest)
     EXPECT_FLOAT_EQ(pwml.read_period(), pulse_period);
     EXPECT_FLOAT_EQ(phase.read_period(), pulse_period);
 
-    // @test Motor::min_pulse_period と異なる値
+    /// @test Motor::min_pulse_period と異なる値
     a3921.pulse_period(Motor::min_pulse_period);
 
     EXPECT_FLOAT_EQ(pwmh.read_period(), Motor::min_pulse_period);
     EXPECT_FLOAT_EQ(pwml.read_period(), Motor::min_pulse_period);
     EXPECT_FLOAT_EQ(phase.read_period(), Motor::min_pulse_period);
 
-    // @test Motor::max_pulse_period と異なる値
+    /// @test Motor::max_pulse_period と異なる値
     a3921.pulse_period(Motor::max_pulse_period);
 
     EXPECT_FLOAT_EQ(pwmh.read_period(), Motor::max_pulse_period);
@@ -363,9 +493,11 @@ TEST(A3921, PulsePeriodTest)
     // 本当のエラー時にエラー出力させたいので、異常系のテスト中は標準エラー出力をキャプチャする
     testing::internal::CaptureStderr();
 
-    /// @test pulse_period が min_pulse_period 未満の場合
     Error& error = Error::get_instance();
     EXPECT_EQ(error.get_status(), Error::Status::Normal);
+
+    /// @test pulse_period が min_pulse_period 未満の場合
+    error.reset();
     a3921.pulse_period(Motor::min_pulse_period - Motor::min_pulse_period * 0.01F);
     EXPECT_EQ(error.get_status(), Error::Status::Error);
 
