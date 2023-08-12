ed4a21e53372c8d8987df217d00ee9552f62c354
yutotnh <57719497+yutotnh@users.noreply.github.com>
2023-08-12 23:34:57 +0000

    docs: 各定数に説明を追加 #157

--- src/spirit/include/version.h
+++ src/spirit/include/version.h
@@ -5,10 +5,16 @@ namespace spirit {
 
 namespace version {
 
+/// バージョン番号のメジャー番号
 constexpr uint32_t major = 0;
+
+/// バージョン番号のマイナー番号
 constexpr uint32_t minor = 1;
+
+/// バージョン番号のパッチ番号
 constexpr uint32_t patch = 0;
 
+/// バージョン番号の文字列
 constexpr char string[] = "0.1.0";
 
 }  // namespace version
